ce49e1966f14859f588d5fcb66c3df08fea8ec1b
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-03-26 14:00:28 +0530

    Firebase config setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: 'AIzaSyBSw1NMgRjX4oJtSBiO1ghzU09Q5vO7AI4',
+	authDomain: 'image-gallery-yogesh.firebaseapp.com',
+	projectId: 'image-gallery-yogesh',
+	storageBucket: 'image-gallery-yogesh.appspot.com',
+	messagingSenderId: '778829803078',
+	appId: '1:778829803078:web:89caf2f731942a85926447',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const storage = firebase.storage()
+const firestore = firebase.firestore()
+
+export { storage, firestore }
